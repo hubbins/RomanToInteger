10925ce4b97cc2476166db125c9793bcf549c10f
Sean O'Connor <soconnor@yahoo.com>
2017-12-12 13:17:00 -0600

    small change

--- RomanToInteger/Solution.cs
+++ RomanToInteger/Solution.cs
@@ -1,17 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Text;
 
 namespace RomanToInteger
 {
     public class Solution
     {
+        private static readonly Dictionary<char, int> Numerals = new Dictionary<char, int>()
+        {
+            {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}
+        };
+
         public int RomanToInt(string s)
         {
-            var Numerals = new Dictionary<char, int>()
-            {
-                {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}
-            };
             var total = 0;  // accumulator
             var i = 0;      // string index
 
@@ -21,7 +23,7 @@ namespace RomanToInteger
             while (i < s.Length)
             {
                 // look at current numeral and next numeral, if current is less process both together
-                if (i < s.Length - 1 && Numerals[s[i]] < Numerals[s[i+1]])
+                if (i < s.Length - 1 && Numerals[s[i]] < Numerals[s[i + 1]])
                 {
                     total += (Numerals[s[i + 1]] - Numerals[s[i]]);
                     i++;    // skip an extra numeral, we processed two
