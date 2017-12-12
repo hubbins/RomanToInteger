1fb281ff162bfc53323986339303f5c88697bd45
Sean O'Connor <soconnor@yahoo.com>
2017-12-12 11:45:06 -0600

    Optimize

--- RomanToInteger/Solution.cs
+++ RomanToInteger/Solution.cs
@@ -6,7 +6,10 @@ namespace RomanToInteger
 {
     public class Solution
     {
-        public enum Numerals { I = 1, V = 5, X = 10, L = 50, C = 100, D = 500, M = 1000 }
+        public readonly Dictionary<char, int> Numerals = new Dictionary<char, int>()
+        {
+            {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000}
+        };
 
         public int RomanToInt(string s)
         {
@@ -19,25 +22,20 @@ namespace RomanToInteger
             while (i < s.Length)
             {
                 // look at current numeral and next numeral, if current is less process both together
-                if (i < s.Length - 1 && GetValue(s[i]) < GetValue(s[i + 1]))
+                if (i < s.Length - 1 && Numerals[s[i]] < Numerals[s[i+1]])
                 {
-                    total += (GetValue(s[i + 1]) - GetValue(s[i]));
+                    total += (Numerals[s[i + 1]] - Numerals[s[i]]);
                     i++;    // skip an extra numeral, we processed two
                 }
                 else
                 {
-                    total += GetValue(s[i]);
+                    total += Numerals[s[i]];
                 }
 
                 i++;
             }
 
             return total;
-        }
-
-        private int GetValue(char numeral)
-        {
-            return (int)Enum.Parse(typeof(Numerals), "" + numeral);
         }
     }
 }
